16d5a286c8a0c81ea02e671dae75bc7c78550227
JefersonAlRo <jefersonrodriguess99@gmail.com>
2022-08-22 21:12:12 -0300

    Some changes

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+//variáveis
 const randomNumber = Math.round(Math.random() * 10)
 let attempts = 1
 const screen1 = document.querySelector('.screen1')
 const screen2 = document.querySelector('.screen2')
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+
+//Eventos
+btnTry.addEventListener('click', handleTryClick) //funcao como referencia
+btnReset.addEventListener('click', handleResetClick)
+
 //função callback
 function handleTryClick(event) {
   event.preventDefault() //por padrão, um  botão tem a tarefa de enviar o formulario, recarregando a pagina. Para evitar isso, usamos um preventDefault que evita a tarefa padrão daquele item
@@ -17,16 +25,12 @@ function handleTryClick(event) {
   inputNumber.value = ''
   attempts++
 }
-
-//eventos
-
-const btnTry = document.querySelector('#btnTry')
-const btnReset = document.querySelector('#btnReset')
-
-btnTry.addEventListener('click', handleTryClick) //funcao como referencia
-btnReset.addEventListener('click', function () {
-  //funcao anonima
-  screen1.classList.remove('hide') //removerá o hide a classe screen1
-  screen2.classList.add('hide') //adicionará o hide da classe screen2
+function handleResetClick(event) {
+  toggleScreen()
   attempts = 1
-})
+}
+function toggleScreen() {
+  //função para dar toggle nas telas, sem precisar de add e remove
+  screen1.classList.toggle('hide')
+  screen2.classList.toggle('hide')
+}
